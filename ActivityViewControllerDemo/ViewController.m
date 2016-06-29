e4179a2375d43a3f33b5270d4e1cc7fa9bde5771
CainLuo <350116542@qq.com>
2016-06-29 09:47:31 +0800

    优化App整体, 开放标题, 以及分享内容
    优化App整体, 开放标题, 以及分享内容

--- ActivityViewControllerDemo/ViewController.m
+++ ActivityViewControllerDemo/ViewController.m
@@ -9,32 +9,87 @@
 #import "ViewController.h"
 #import "AspActivityViewController.h"
 
-@interface ViewController ()
+#define CAL_GET_OBJECT(objc) if (objc) return objc
+
+@interface ViewController () <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *shareTitle;
+@property (weak, nonatomic) IBOutlet UITextField *shareContent;
+
+@property (nonatomic, strong) AspActivityViewController *activityViewController;
+
+@property (nonatomic, strong) NSArray *items;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];    
+    [super viewDidLoad];
+    
+    NSString *textItem = self.shareTitle.text;
+    UIImage *imageToShare = [UIImage imageNamed:@"Icon-60"];
+    
+    self.items = @[textItem, imageToShare];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
 }
 
 - (IBAction)presentAction:(UIButton *)sender {
+    [self.view endEditing:YES];
+
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"温馨提示"
+                                                        message:@""
+                                                       delegate:nil
+                                              cancelButtonTitle:@"好"
+                                              otherButtonTitles:nil, nil];
     
-    NSString *textItem = @"Check out the yourAppNameHere app: itunes http link to your app here";
-    UIImage *imageToShare = [UIImage imageNamed:@"wechat_session"];
+
+    if (self.shareTitle.text.length <= 0) {
+        
+        alertView.message = @"您尚未输入需要分享的标题, 请稍后重试.";
+        [alertView show];
+        
+        return;
+    }
     
-    NSArray *items = @[textItem, imageToShare];
+    if (self.shareContent.text.length <= 0) {
+
+        alertView.message = @"您尚未输入需要分享的内容, 请稍后重试.";
+
+        [alertView show];
+        
+        return;
+    }
     
-    AspActivityViewController *activityVC = [[AspActivityViewController alloc] initAspActivityControllerWithContent:items];
+    [self presentViewController:self.activityViewController animated:YES completion:nil];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
     
-    activityVC.customShareMessage.shareTitle = @"腾讯QQ分享";
-    activityVC.customShareMessage.shareLink = @"http://www.qq.com";
-    activityVC.customShareMessage.shareImage = [UIImage imageNamed:@"wechat_timeline"];
-    activityVC.customShareMessage.shareThumbnail = [UIImage imageNamed:@"wechat_timeline"];
-    activityVC.customShareMessage.shareDescription = [NSString stringWithFormat:@"这里写的是message.description %f", [[NSDate date] timeIntervalSince1970]];
+    if (textField.tag == 0) {
+
+        self.activityViewController.customShareMessage.shareTitle = [NSString stringWithFormat:@"标题: %@", textField.text];
+    } else {
+     
+        self.activityViewController.customShareMessage.shareDescription = [NSString stringWithFormat:@"介绍内容: %@", textField.text];
+    }
+}
+
+- (AspActivityViewController *)activityViewController {
+
+    CAL_GET_OBJECT(_activityViewController);
     
-    [self presentViewController:activityVC animated:YES completion:nil];
+    _activityViewController = [[AspActivityViewController alloc] initAspActivityControllerWithContent:self.items];
+    
+    _activityViewController.customShareMessage.shareLink = @"http://www.apple.com.cn";
+
+    _activityViewController.customShareMessage.shareImage     = [UIImage imageNamed:@"Icon-60"];
+    _activityViewController.customShareMessage.shareThumbnail = [UIImage imageNamed:@"Icon-60"];
+    
+    return _activityViewController;
 }
 
 @end
